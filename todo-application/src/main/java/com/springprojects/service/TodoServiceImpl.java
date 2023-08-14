079fc69e51a34068891489ddf70c2de0cba4820b
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-08-14 12:34:46 +0530

    Use ModelMapper Library

--- todo-application/src/main/java/com/springprojects/service/TodoServiceImpl.java
+++ todo-application/src/main/java/com/springprojects/service/TodoServiceImpl.java
@@ -4,6 +4,7 @@ import com.springprojects.dto.TodoDto;
 import com.springprojects.entity.Todo;
 import com.springprojects.repository.TodoRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,19 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService{
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
+
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         // Convert TodoDto into Todo JPA Entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
         // Save JPA Entity into Database
         Todo savedTodo = todoRepository.save(todo);
 
         // Convert savedTodo into TodoDto
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
     }
