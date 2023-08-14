51cf738bcb37ccfa91113842830ed22bf8b56a70
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-08-14 11:48:00 +0530

    Setup Spring Boot Project

---
+++ todo-application/src/test/java/com/springprojects/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
