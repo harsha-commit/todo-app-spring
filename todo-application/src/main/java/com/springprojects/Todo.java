b26602cad8ec43d6b85c9994d29221ceb2a59a18
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-08-14 12:00:25 +0530

    Create Todo JPA Entity

--- todo-application/src/main/java/com/springprojects/Todo.java
+++ todo-application/src/main/java/com/springprojects/Todo.java
@@ -1,2 +1,28 @@
-package com.springprojects;public class Todo {
+package com.springprojects;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    // boolean and primary key - by default not nullable
+    private boolean completed;
 }
