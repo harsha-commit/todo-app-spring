f3295fe7fe57321a5e2e9639b12d11d02f2c8233
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-08-28 12:33:23 +0530

    Implement In Memory Authentication

--- todo-application/src/main/java/com/springprojects/config/SpringSecurityConfig.java
+++ todo-application/src/main/java/com/springprojects/config/SpringSecurityConfig.java
@@ -5,10 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    // Password Encoder
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     // Enabling ONLY HttpBasic Authentication
     // NOT RECOMMENDED FOR PRODUCTION
     // Authentication is done by sending base64 encoded in headers, which can be easily decoded
@@ -25,5 +38,25 @@ public class SpringSecurityConfig {
         });
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetails(){
+
+        UserDetails harsha = User.builder()
+                .username("harsha")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                // Expects password in Encoded format, not plain text
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        // Storing the UserDetails in Spring Security provided In Memory Object
+        return new InMemoryUserDetailsManager(harsha, admin);
     }
 }
